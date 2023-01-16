deaf431d326a2d51d1f485c44a1ca433ad774739
sumitradas94 <sumitradas94@gmail.com>
2023-01-16 10:59:07 +0530

    methodcalling

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Welcome to DataStructure and Algorithm Practise Problem");
+
+            String str = "Best!";
+            int n = str.Length;
+            Console.Write("All possible strings of : ");
+            PermutationofString.usingIterativeMethod(str, 0, n - 1);
 
         }
     }
