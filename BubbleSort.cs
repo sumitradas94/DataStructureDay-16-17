9e36547b94afc385d6560f903550c012a12639e8
sumitradas94 <sumitradas94@gmail.com>
2023-01-18 09:42:48 +0530

    Bubble Sort

---
+++ BubbleSort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProgram
+{
+    public class BubbleSort
+    {
+        public void Display()
+        {
+
+
+
+            int[] arr = { 78, 55, 45, 98, 13 };
+            Console.WriteLine("Before Sorting Array elements Are : ");
+
+            foreach (int p in arr)
+            {
+                Console.WriteLine(p);
+            }
+            Console.WriteLine();
+
+            int temp;
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("After Sorting Array elements Are : ");
+
+            foreach (int p in arr)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
+
