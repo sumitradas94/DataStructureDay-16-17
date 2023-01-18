6b9f901c406a904397d957af58427be94c4465c6
sumitradas94 <sumitradas94@gmail.com>
2023-01-18 09:47:18 +0530

    method calling for UC5

--- Program.cs
+++ Program.cs
@@ -4,14 +4,19 @@
     {
         public static void Main(string[] args)
         {
-           
+            int[] arr = { 22, 11, 15, 5, 45, 7 };
+            Console.WriteLine("Given Array");
+            MergeSort.printArray(arr);
+            MergeSort obja = new MergeSort();
+            obja.sort(arr, 0, arr.Length - 1);
+            Console.WriteLine("\nSorted array");
+            MergeSort.printArray(arr);
 
-           
-
- BubbleSort bubbleSort = new BubbleSort();
- bubbleSort.Display();
-
-
+        }
+    }
 }
-}
-}
+
+
+
+
+ 
