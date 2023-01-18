4a6c5534b4211c42b76de36bc0244097b99dc1df
sumitradas94 <sumitradas94@gmail.com>
2023-01-18 09:43:13 +0530

    Method calling for UC4

--- Program.cs
+++ Program.cs
@@ -4,20 +4,14 @@
     {
         public static void Main(string[] args)
         {
-            /*Console.WriteLine("Welcome to DataStructure and Algorithm Practise Problem");
+           
 
-           String str = "Best!";
-            int n = str.Length;
-            Console.Write("All possible strings of : ");
-            PermutationofString.usingIterativeMethod(str, 0, n - 1);*/
+           
 
-            BinarySearch d = new BinarySearch();
-            d.List();
-            Console.WriteLine("\nEnter word do you want to search : ");
-            string Word = Console.ReadLine();
-            d.Searching(Word);
+ BubbleSort bubbleSort = new BubbleSort();
+ bubbleSort.Display();
 
 
-        }
-    }
+}
+}
 }
