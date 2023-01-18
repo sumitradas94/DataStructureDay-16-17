ce8de02921e923628c80161cf816a9dbb3e6fa1b
sumitradas94 <sumitradas94@gmail.com>
2023-01-18 09:31:56 +0530

    Binary Search Word From wordList

---
+++ BinarySearch.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProgram
+{
+    public class BinarySearch
+    {
+        string[] Arr = { "Sona", "mona", "Priya", "Riya", "Sandeep", "Reeni", "Jini", "sipi", };
+        public void List()
+        {
+            Console.WriteLine("Before Sorting List of word :");
+            Console.Write("[ ");
+            foreach (string str in Arr)
+            {
+
+                Console.Write(str + ", ");
+
+            }
+            Console.WriteLine("]");
+            Console.WriteLine();
+            Array.Sort(Arr);
+            Console.WriteLine("\nAfter sorting List of word :");
+            Console.Write("[ ");
+            foreach (string str in Arr)
+            {
+
+                Console.Write(str + " ");
+
+            }
+            Console.WriteLine("]");
+            Console.WriteLine();
+        }
+        public void Searching(string W)
+        {
+            int c = 0;
+            foreach (string str in Arr)
+            {
+                if (W == str)
+                {
+                    Console.WriteLine("\nWord Found  " + W + "==" + str);
+                    Console.WriteLine("\nPosition of Word in index : " + (Array.BinarySearch(Arr, W) - 1));
+                    c = 1;
+                }
+
+            }
+            if (c == 0)
+            {
+                Console.WriteLine("\nWord Not Found");
+            }
+
+
+        }
+    }
+}
+
