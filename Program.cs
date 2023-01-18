47189350bfc97aba83f892a391eceddad54d96b0
sumitradas94 <sumitradas94@gmail.com>
2023-01-18 09:32:32 +0530

    Method callinf for BinarySearch_UC2

--- Program.cs
+++ Program.cs
@@ -4,12 +4,19 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to DataStructure and Algorithm Practise Problem");
+            /*Console.WriteLine("Welcome to DataStructure and Algorithm Practise Problem");
 
-            String str = "Best!";
+           String str = "Best!";
             int n = str.Length;
             Console.Write("All possible strings of : ");
-            PermutationofString.usingIterativeMethod(str, 0, n - 1);
+            PermutationofString.usingIterativeMethod(str, 0, n - 1);*/
+
+            BinarySearch d = new BinarySearch();
+            d.List();
+            Console.WriteLine("\nEnter word do you want to search : ");
+            string Word = Console.ReadLine();
+            d.Searching(Word);
+
 
         }
     }
