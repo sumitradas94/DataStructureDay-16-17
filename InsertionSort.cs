32da18aa68e13d163739462762836397768878d1
sumitradas94 <sumitradas94@gmail.com>
2023-01-18 09:38:29 +0530

    Insertion sort

---
+++ InsertionSort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProgram
+{
+    public class InsertionSort
+    {
+        public static void Display()
+        {
+            int[] arr = new int[] { 23, 9, 85, 12, 99 };
+            int n = 5, i, j, val, flag;
+            Console.WriteLine("Insertion Sorting: ");
+            Console.WriteLine("Initial Array: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            for (i = 1; i < n; i++)
+            {
+                val = arr[i];
+                flag = 0;
+                for (j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (val < arr[j])
+                    {
+                        arr[j + 1] = arr[j];
+                        j--;
+                        arr[j + 1] = val;
+                    }
+                    else
+                    {
+                        flag = 1;
+                    }
+                }
+                Console.WriteLine("Sorted Array: ");
+
+                for (i = 0; i < n; i++)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+
+
+            }
+        }
+    }
+}
