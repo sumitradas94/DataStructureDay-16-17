54bf0e28b57497abf25bf4d560a335491a6c0a2e
sumitradas94 <sumitradas94@gmail.com>
2023-01-20 10:22:54 +0530

    Method calling for C6

--- Program.cs
+++ Program.cs
@@ -4,13 +4,15 @@
     {
         public static void Main(string[] args)
         {
-            int[] arr = { 22, 11, 15, 5, 45, 7 };
-            Console.WriteLine("Given Array");
-            MergeSort.printArray(arr);
-            MergeSort obja = new MergeSort();
-            obja.sort(arr, 0, arr.Length - 1);
-            Console.WriteLine("\nSorted array");
-            MergeSort.printArray(arr);
+            // int[] arr = { 22, 11, 15, 5, 45, 7 };
+            //  Console.WriteLine("Given Array");
+            //  MergeSort.printArray(arr);
+            //  MergeSort obja = new MergeSort();
+            // obja.sort(arr, 0, arr.Length - 1);
+            // Console.WriteLine("\nSorted array");
+            // MergeSort.printArray(arr);
+
+            Anagram.Display();
 
         }
     }
