f15155276c170c95fc2b6338d0ed916a115e30a1
sumitradas94 <sumitradas94@gmail.com>
2023-01-20 10:22:29 +0530

    Anagram Detection

---
+++ Anagram.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProgram
+{
+    public class Anagram
+    {
+        public static void Display()
+        {
+            string str1 = "heart";
+            string str2 = "traeh";
+
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            string val1 = new string(ch1);
+            string val2 = new string(ch2);
+
+            if (val1 == val2)
+            {
+                Console.WriteLine("Given Input string is Anagram.");
+            }
+
+            else
+            {
+                Console.WriteLine("This is not an Anagram");
+
+            }
+        }
+    }
+}
