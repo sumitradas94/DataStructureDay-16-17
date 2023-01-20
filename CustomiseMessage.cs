5265301a6a21d3e0d4a0482972468b623c6032d1
sumitradas94 <sumitradas94@gmail.com>
2023-01-20 10:53:36 +0530

    Customize Message

---
+++ CustomiseMessage.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AlgorithmProgram
+{
+    public class CustomiseMessage
+    {
+        public static string Reg_pattern1 = "^[a-zA-Z]+$";
+        public static string Reg_pattern2 = "^[a-zA-Z]+[' ']{1}[a-zA-Z]+$";
+        public static string Reg_pattern3 = "^[0-9]{10}$";
+        public static string Reg_pattern4 = "^[0-9]{2}[/]{1}[0-9]{2}[/]{1}[0-9]{4}$";
+        public void Display()
+        {
+            Console.WriteLine("enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("enter your full name");
+            string fname = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("enter your mobile number");
+            string mb = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("enter date in formate dd/mm/yyyy");
+            string dt = Console.ReadLine();
+            Console.WriteLine();
+
+
+
+            bool val1 = Regex.IsMatch(name, Reg_pattern1);
+            bool val2 = Regex.IsMatch(fname, Reg_pattern2);
+            bool val3 = Regex.IsMatch(mb, Reg_pattern3);
+            bool val4 = Regex.IsMatch(dt, Reg_pattern4);
+
+
+
+
+            if (val1 && val2 & val3 & val4)
+            {
+                Console.WriteLine("Hello {0},We have your fullname as {1} in our system. your contact number is 91-{2}.\r\n" +
+                        "Please,let us know in case of any clarification Thank you BridgeLabz {3}.", name, fname, mb, dt);
+            }
+            else
+                Console.WriteLine("pattern is wrong");
+        }
+    }
+}
+    
